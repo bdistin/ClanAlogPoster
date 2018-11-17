22106ee52b88bf053d3d76c859d1675f974a8171
Unknown <bdistin@gmail.com>
2018-11-17 13:29:52 -0600

    lint, fix one bug

--- src/ClanMember.js
+++ src/ClanMember.js
@@ -6,80 +6,80 @@ const fetch = require('node-fetch');
  */
 class ClanMember {
 
-    /**
+	/**
      * @typedef {object} Activity
      * @property {string} date The date of the activity
      * @property {string} text The text of the activity
      * @property {string} description The description of the activity
      */
 
-    /**
+	/**
      * @param {Client} client The client that manages this ClanMember
      * @param {string} name The name of the clan member
      * @param {string} [lastEvent] The last event we have fetched for this member
      */
-    constructor(client, name, lastEvent) {
-        /**
+	constructor(client, name, lastEvent) {
+		/**
          * The Client
          * @type {Client}
          */
-        this.client = client;
+		this.client = client;
 
-        /**
+		/**
          * The clan members name
          * @type {string}
          */
-        this.name = name;
+		this.name = name;
 
-        /**
+		/**
          * The last event we have displayed for this clan member
          * @type {?Date}
          */
-        this.lastEvent = lastEvent ? new Date(lastEvent) : null;
+		this.lastEvent = lastEvent ? new Date(lastEvent) : null;
 
-        /**
+		/**
          * The number of fetch errors we have had for this clan member (resets on a success)
          * @type {number}
          */
-        this.errorsFetching = 0;
-    }
+		this.errorsFetching = 0;
+	}
 
-    /**
+	/**
      * If we consider this clan member active
      * @type {boolean}
      */
-    get active() {
-        return this.errorsFetching < this.client.errorLimit;
-    }
+	get active() {
+		return this.errorsFetching < this.client.errorLimit;
+	}
 
-    /**
+	/**
      * Gets activities for this clan member
      * @returns {Activity[]}
      */
-    async getActivities() {
-        try {
-            const data = await fetch(`https://apps.runescape.com/runemetrics/profile/profile?user=${encodeURIComponent(this.name)}&activities=20`).then(res => res.json());
-            if (data.error) throw data.error;
-            this.errorsFetching = 0;
-            const activities = data.activities.filter(activity => new Date(activity.date) > (this.lastEvent || 0)).reverse();
-            if (activities.length) this.lastEvent = new Date(activities[activities.length - 1].date);
-            return activities;
-        } catch (err) {
-            this.errorsFetching++;
-            return [];
-        }
-    }
+	async getActivities() {
+		try {
+			const data = await fetch(`https://apps.runescape.com/runemetrics/profile/profile?user=${encodeURIComponent(this.name)}&activities=20`).then(res => res.json());
+			if (data.error) throw data.error;
+			this.errorsFetching = 0;
+			const activities = data.activities.filter(activity => new Date(activity.date) > (this.lastEvent || 0)).reverse();
+			if (activities.length) this.lastEvent = new Date(activities[activities.length - 1].date);
+			return activities;
+		} catch (err) {
+			this.errorsFetching++;
+			return [];
+		}
+	}
 
-    /**
+	/**
      * Defines the json.stringify behavior of class instances
-     * @returns {object}
+     * @returns {Object}
      */
-    toJSON() {
-        return {
-            name: this.name,
-            lastEvent: this.lastEvent && this.lastEvent.toString()
-        }
-    }
+	toJSON() {
+		return {
+			name: this.name,
+			lastEvent: this.lastEvent && this.lastEvent.toString()
+		};
+	}
 
 }
 
--- src/Client.js
+++ src/Client.js
@@ -6,7 +6,7 @@ const fs = require('fs-nextra');
 // Import the node-fetch module
 const fetch = require('node-fetch');
 // Import promisify from node.js's util
-const { promisify } = require('util')
+const { promisify } = require('util');
 // Define an async timout
 const sleep = promisify(setTimeout);
 
@@ -17,133 +17,133 @@ const ClanMember = require('./ClanMember');
  */
 class Client {
 
-    /**
-     * @param {object} config The configuration for this Client
+	/**
+     * @param {Object} config The configuration for this Client
      * @property {string} config.webhookID The id of the webhook to post to
      * @property {string} config.webhookToken The token for posting to the webhook
      * @property {string} config.clan The name of the clan to post activities from
      * @property {number} config.errorLimit The number of errors before the client starts skipping checking them
      */
-    constructor(config) {
-        /**
+	constructor(config) {
+		/**
          * The webhook connection to post to
          * @type {external:WebhookClient}
          */
-        this.webhook = new Discord.WebhookClient(config.webhookID, config.webhookToken);
+		this.webhook = new Discord.WebhookClient(config.webhookID, config.webhookToken);
 
-        /**
+		/**
          * The name of the clan to post activities from
          * @type {string}
          */
-        this.clanName = config.clan;
+		this.clanName = config.clan;
 
-        /**
+		/**
          * The ClanMember cache
          * @type {Map<string, ClanMember>}
          */
-        this.clanMembers = new Map();
+		this.clanMembers = new Map();
 
-        /**
+		/**
          * The number of errors before the client starts skipping checking them
          * @type {number}
          */
-        this.errorLimit = config.errorLimit;
+		this.errorLimit = config.errorLimit;
 
-        this.webhook.send('I am now alive!');
-    }
+		this.webhook.send('I am now alive!');
+	}
 
-    /**
+	/**
      * Starts this client checking for activities
      * @returns {Promise<void>}
      */
-    async start() {
-        try {
-            const members = await fs.readJSON('./clanmembers.json');
-            for (const member of members) this.createMember(member.name, member.lastEvent);
-        } catch (err) {
-            await fs.createFileAtomic('./clanmembers.json');
-        }
-        return this.loop();
-    }
+	async start() {
+		try {
+			const members = await fs.readJSON('./clanmembers.json');
+			for (const member of members) this.createMember(member.name, member.lastEvent);
+		} catch (err) {
+			await fs.createFileAtomic('./clanmembers.json');
+		}
+		return this.loop();
+	}
 
-    /**
+	/**
      * Creates and caches a new ClanMember to track activities
      * @param {string} name The name of the clan member
      * @param {string} [lastEvent] The last event recorded from them
      * @returns {void}
      */
-    createMember(name, lastEvent) {
-        this.clanMembers.set(name, new ClanMember(this, name, lastEvent));
-    }
+	createMember(name, lastEvent) {
+		this.clanMembers.set(name, new ClanMember(this, name, lastEvent));
+	}
 
-    /**
+	/**
      * Saves the current cache to file, so activites are not posted multiple times
      * @returns {Promise<void>}
      */
-    async save() {
-        await fs.writeJSONAtomic('./clanmembers.json', [...this.clanMembers.values()]);
-    }
+	async save() {
+		await fs.writeJSONAtomic('./clanmembers.json', [...this.clanMembers.values()]);
+	}
 
-    /**
+	/**
      * Sleeps an amount of time between api requests
      * @returns {Promise<void>}
      */
-    async sleep() {
-        // sleep 5 seconds plus a random fraction of a second
-        await sleep(5000 + (Math.random() * 1000));
-    }
+	async sleep() {
+		// sleep 5 seconds plus a random fraction of a second
+		await sleep(5000 + (Math.random() * 1000));
+	}
 
-    /**
+	/**
      * The Activity checking loop
      * @returns {Promise<void>}
      */
-    async loop() {
-        console.log('getting members');
-        await this.getMemberList();
-        console.log('looping members');
-        await this.loopMembers();
-        return this.loop();
-    }
+	async loop() {
+		console.log('getting members');
+		await this.getMemberList();
+		console.log('looping members');
+		await this.loopMembers();
+		return this.loop();
+	}
 
-    /**
+	/**
      * Gets the clan member list from the api and updates the local cache
      * @returns {Promise<void>}
      */
-    async getMemberList() {
-        try {
-            const membersString = await fetch(`http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=${encodeURIComponent(this.clanName)}`).then(res => res.text());
-            const members = membersString.split('\n').map(memberString => memberString.split(',')[0].replace(' ', '_')).slice(1);
+	async getMemberList() {
+		try {
+			const membersString = await fetch(`http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName=${encodeURIComponent(this.clanName)}`).then(res => res.text());
+			const members = membersString.split('\n').map(memberString => memberString.split(',')[0].replace(' ', '_')).slice(1);
 
-            // Removes any members that may not be in the clan any longer
-            const membersClone = new Map(this.clanMembers);
-            this.clanMembers.clear();
+			// Removes any members that may not be in the clan any longer
+			const membersClone = new Map(this.clanMembers);
+			this.clanMembers.clear();
 
-            for (const member of members) {
-                const existing = membersClone.get(member);
+			for (const member of members) {
+				const existing = membersClone.get(member);
 
-                if (existing) this.clanMembers.set(existing.name, existing);
-                else this.createMember(member);
-            }
-        } catch (error) {
-            console.error(error);
-            process.exit();
-        }
-        return this.sleep();
-    }
+				if (existing) this.clanMembers.set(existing.name, existing);
+				else this.createMember(member);
+			}
+		} catch (error) {
+			console.error(error);
+			process.exit();
+		}
+		return this.sleep();
+	}
 
-    /**
+	/**
      * Loops through the members and gets their activities
      * @returns {Promise<void>}
      */
-    async loopMembers() {
-        for (const member of this.clanMembers.values()) {
-            if (!member.active) continue;
-            const activities = await member.getActivities();
-            if (activities.length) await hook.send(activities.map(activity => `${member.name}: ${activity.text}`).join('\n'));
-            await this.save();
-            await this.sleep();
-        }
-    }
+	async loopMembers() {
+		for (const member of this.clanMembers.values()) {
+			if (!member.active) continue;
+			const activities = await member.getActivities();
+			if (activities.length) await this.webhook.send(activities.map(activity => `${member.name}: ${activity.text}`).join('\n'));
+			await this.save();
+			await this.sleep();
+		}
+	}
 
 }
 
